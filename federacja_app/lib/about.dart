36c5361157e325a23eabbfaad6a87f70a33a5cb6
mikki <mikki@nenna.is>
2022-08-24 21:44:26 +0200

    Added: News downloading from DB

--- federacja_app/lib/about.dart
+++ federacja_app/lib/about.dart
@@ -210,9 +210,8 @@ class AboutCommitteeState extends State<AboutCommittee>
                                   ));
                             }))
                   ])
-            : Container(
-                child: const Center(
-                    child: Text(
-                        'Problem loading section, please contact the dev team.'))));
+            : const Center(
+                child: Text(
+                    'Problem loading section, please contact the dev team.')));
   }
 }
