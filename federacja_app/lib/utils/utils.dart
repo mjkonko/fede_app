03b42fa50bc170b39ec9db7ad685c59d745ac99d
mikki <mikki@nenna.is>
2022-10-24 17:31:24 +0200

    Fixed: Android build & link launches

--- federacja_app/lib/utils/utils.dart
+++ federacja_app/lib/utils/utils.dart
@@ -9,7 +9,10 @@ import '../entity/event_link_instance.dart';
 
 class Utils {
   Future<void> sendEmail({required String email}) async {
-    final Uri params = Uri(scheme: 'mailto', path: email);
+    final Uri params = Uri(
+        scheme: 'mailto',
+        path: email,
+        queryParameters: {'subject': '', 'body': ''});
 
     if (await ul.canLaunchUrl(params)) {
       await ul.launchUrl(params, mode: ul.LaunchMode.externalApplication);
@@ -18,37 +21,13 @@ class Utils {
     }
   }
 
-  Future<void> openUrl({required String url}) async {
-    Uri uri = Uri.parse(url);
-    if (await ul.canLaunchUrl(uri)) {
-      await ul.launchUrl(uri);
-    } else {
-      throw Exception("Unable to open the browser");
-    }
-  }
-
   Future<void> launchInBrowser(String url) async {
     Uri uri = Uri.parse(url);
     if (!await launchUrl(
       uri,
-      mode: LaunchMode.externalApplication,
+      mode: LaunchMode.externalNonBrowserApplication,
     )) {
       throw 'Could not launch $url';
-    }
-  }
-
-  Future<void> launchUniversalLinkIos(String url) async {
-    Uri uri = Uri.parse(url);
-
-    final bool nativeAppLaunchSucceeded = await launchUrl(
-      uri,
-      mode: LaunchMode.externalNonBrowserApplication,
-    );
-    if (!nativeAppLaunchSucceeded) {
-      await launchUrl(
-        uri,
-        mode: LaunchMode.inAppWebView,
-      );
     }
   }
 
